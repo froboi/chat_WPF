ed1ddda06b99242997c251dbec66f8a5d223da4f
Tommaso <casaletommi@gmail.com>
2024-11-15 12:01:00 +0100

    finto xaml da finire c#
    interfaccia grafica finita da finire c# aggiungere timer

--- chatInterfacciaGrafica/chatInterfacciaGrafica/MainWindow.xaml.cs
+++ chatInterfacciaGrafica/chatInterfacciaGrafica/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace chatInterfacciaGrafica
 {
@@ -16,9 +19,74 @@ namespace chatInterfacciaGrafica
     /// </summary>
     public partial class MainWindow : Window
     {
-            public MainWindow()
+
+        string ultimoMess = "";
+
+        public MainWindow()
         {
             InitializeComponent();
         }
+
+        private string StartCLient
+            (string messaggio, string indirizzoIP)
+        {
+            string risposta = "";
+            byte[] bytes = new byte[2048];
+            IPAddress ipAddress = IPAddress.Parse(indirizzoIP);
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            try
+            {
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    sender.Connect(remoteEP);
+                    byte[] msg = Encoding.UTF8.GetBytes(messaggio);
+                    int bytesSent = sender.Send(msg);
+                    int bytesRec = sender.Receive(bytes);
+                    risposta = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                    return risposta;
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return risposta;
+        }
+
+        private void chatJoin_Click(object sender, RoutedEventArgs e)
+        {
+            string messaggio = TxtNickname.Text + "è entrato nella chat";
+            string indirizzoIP = TxtIndirizzoIP.Text;
+            string risposta = StartCLient(messaggio, indirizzoIP);
+        }
+
+        private void chatSend_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+        
+
+
+
+
+
+
     }
 }
